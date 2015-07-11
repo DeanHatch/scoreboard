1abc54af3dd794d7939439e6d60bf1cdb5b26df1
Dean Hatch <dean.hatch@milehighsoftware.com>
2015-07-11 11:28:14 -0400

    Welcome page changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
 
   def nav_link_array()
-	  [ navitem('View a Competition' , :choose_display_customer) ,
-  	     navitem('Report/Correct Scores' , :scorer_choose_customer) ,
+	  [navitem('Report/Correct Scores' , :scorer_choose_customer) ,
 	navitem('Manage a Competition' , :choose_customer_manager),
 	navitem('Customer Login/Register', :new_customer_session) ]
   end
