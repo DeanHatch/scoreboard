e9d0b3d0cc88cae91d3642eae849a38c1b732c25
Dean Hatch <dean.hatch@milehighsoftware.com>
2016-08-26 15:31:59 -0400

    Configured EMail

---
+++ app/assets/javascripts/valid_times.js
@@ -0,0 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+function doNotEndBeforeStart() {
+    document.getElementById("valid_time_to_time").value =
+ 	document.getElementById("valid_time_from_time").value;
+}
